c24d7be64cca5ee6328920b39dc6bf40af4630dc
John Rampelt <johnrampelt@gmail.com>
2025-03-01 16:36:20 -0500

    Update Breakout.py with sound

--- LCD/Breakout.py
+++ LCD/Breakout.py
@@ -202,12 +202,16 @@ while(True):
                         ball_y_velocity = -ball_y_velocity
                         bricks_remaining -= 1
                         score += 10 * speed
+                        BEEPER.freq(1000)
+                        BEEPER.duty_u16(32768)
         
         # Check for paddle hit
         if paddle_x - half_paddle_w < ball_x + 2 and ball_x - 2 < paddle_x + half_paddle_w:
             if paddle_y + 2 > ball_y > paddle_y - 4:
                 ball_y_velocity = -ball_y_velocity
                 ball_y = ball_y + ball_y_velocity
+                BEEPER.freq(500)
+                BEEPER.duty_u16(32768)
                 
         # Check for ball drain
         if ball_y >= lcd.height:
@@ -281,7 +285,9 @@ while(True):
     skip -= 1
     if skip == 0:
         lcd.update()
+        BEEPER.duty_u16(0)
         skip = 2
 
     # Update LCD every frame on BEAPER Nano
     # lcd.update()
+    # BEEPER.duty_u16(0)
