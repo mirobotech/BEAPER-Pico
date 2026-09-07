67b977f6270933713f296f8a396b3356eb084e7c
John Rampelt <johnrampelt@gmail.com>
2026-09-07 15:55:54 -0400

    Update Activity_B01_Output.py

--- Beginner Activities/Activity_B01_Output.py
+++ Beginner Activities/Activity_B01_Output.py
@@ -19,7 +19,9 @@ while True:
     pass
 
 
-# # Guided Exploration
+# ================================================================================
+# Guided Exploration
+# ================================================================================
 #
 # Guided exploration (GE) activities and questions highlight the key ideas
 # introduced in the program. In a classroom, instructors can use them to
@@ -31,104 +33,84 @@ while True:
 # MicroPython programs. In certain guided exploration activities, you'll
 # be asked to modify or add code to the starter program presented above.
 #
-# The contents of this entire comment block are formatted in Markdown
-# syntax - with the exception of the very first comment pound # tag at
-# the start of every line (these are just regular MicroPython comments).
-# To extract these guided exploration and extension activities sections
-# into a Markdown file, highlight the text in these sections and select
-# `Toggle comment` from the `Edit` menu in Thonny.
-#
-# Example program code will be presented like this:
-#
-# **Example code:**
-# ```python
-# beaper.LED2.value(1)
-# ```
-#
-# Double-asterisks are Markdown syntax for bold text, so a Markdown editor
-# would display the words `Example code:` bolded. The three backtick (```)
-# characters, often followed by a language description like `python` or
-# `cpp` are used to format code blocks, making it stand out. They are not
-# part of the code, so when you need to copy the example text, just select
-# the lines of text between the sets of backtick. In this example the code
-# being referenced is the single line instruction: `beaper.LED2.value(1)`.
-#
-# Words shown in single backticks within the guided exploration sections,
-# like `LED2`, are also code - they show you the exact spelling and
-# capitalization to use. Are you ready to start exploring? Let's go!
-#
-# ## GE 1 - Comments and program statements
+# --------------------------------------------------------------------------------
+# GE 1 - Comments and program statements
+# --------------------------------------------------------------------------------
 #
 # Most computer programs contain both text comments (which help anyone
 # reading the program code to understand it) and the actual program
 # statements that the microcontroller will run or execute.
 #
-# Comments in MicroPython follow a pound sign `#`, meaning everything that
+# Comments in MicroPython follow a pound sign '#', meaning everything that
 # follows a pound sign in a line of text will be ignored.
 #
 # In the starter program, most of the text is comments. How does the
 # MicroPython editor you're using treat the comments differently from the
 # actual program code?
 #
-# ## GE 2 - Importing the board module
+# --------------------------------------------------------------------------------
+# GE 2 - Importing the board module
+# --------------------------------------------------------------------------------
 #
 # The first real program statement encountered in this program imports a
 # board module file for your BEAPER Pico circuit.
 #
-# **Example code:**
-# ```python
+# Example code:
+#
 # import BEAPER_Pico as beaper
-# ```
 #
-# The `BEAPER_Pico.py` board module file must be copied into the filesystem
-# memory of the microcontroller before this program runs. If the file is not
-# present when the import statement runs, an error will be generated, and an
-# error message will be printed in the shell window below.
+# The 'BEAPER_Pico.py' board module file must be copied into the filesystem
+# memory of the microcontroller before this program runs. If the file is
+# not present when the import statement runs, an error will be generated,
+# and an error message will be printed in the shell window below.
 #
-# The `BEAPER_Pico.py` board module file contains ordinary MicroPython
+# The BEAPER_Pico.py board module file contains ordinary MicroPython
 # program code that defines and configures BEAPER Pico's I/O devices and
-# microcontroller pins for you, helping you to start writing programs more
-# quickly. You can open the board module file in the code editor to explore
-# all of the code it contains.
+# microcontroller pins for you, helping you to start writing programs
+# more quickly. You can open the board module file in the code editor to
+# explore all of the code it contains.
 #
 # The board module file includes the names and pin numbers of all I/O
-# devices on the circuit, including `LED2`. This lets program statements,
-# like the one below, use the name `LED2` to control its operation
-# instead of us needing to know the pin number LED2 is connected to.
+# devices on the circuit, including LED2. This lets program statements,
+# like the one below, use the name LED2 to control its operation instead
+# of us needing to know the pin number LED2 is connected to.
 #
-# **Example code:**
-# ```python
+# Example code:
+#
 # beaper.LED2.value(1)
-# ```
 #
-# Let's break this statement down: the `BEAPER_Pico` board module has been
-# given the shorter name `beaper` by the import statement, and `LED2` is
-# one of the objects defined in the `BEAPER_Pico` board module file.
-# MicroPython's `value` method is used to read or set pin values, so the
+# Let's break this statement down: the BEAPER_Pico board module has been
+# given the shorter name 'beaper' by the import statement, and 'LED2' is
+# one of the objects defined in the BEAPER_Pico board module file.
+# MicroPython's 'value' method is used to read or set pin values, so the
 # end result of the microcontroller running this statement is that the
-# value of the `LED2` pin gets set to `1`, meaning the output voltage of the
+# value of the LED2 pin gets set to 1, meaning the output voltage of the
 # pin connected to LED2 is set to around 3.3V, which will turn LED2 on.
 #
-# All of BEAPER Pico's other LEDs (`LED2` - `LED5`) can be controlled in
+# All of BEAPER Pico's other LEDs (LED2 - LED5) can be controlled in
 # exactly the same way. Let's try it.
 #
 # Add a second LED output statement to light LED3 below the existing
 # statement that lights LED2. Run the updated program to verify that it
 # works as expected.
 #
-# ## GE 3 - Sequential execution
+# --------------------------------------------------------------------------------
+# GE 3 - Sequential execution
+# --------------------------------------------------------------------------------
 #
 # MicroPython runs every program by reading, interpreting, and executing
-# each program statement, in order, from the top of the program code to the
-# bottom.
+# each program statement, in order, from the top of the program code to
+# the bottom.
 #
-# So, after adding the statement `beaper.LED3.value(1)` below the existing
-# `beaper.LED2.value(1)` statement in GE 2, the microcontroller will turn
+# So, after adding the statement 'beaper.LED3.value(1)' below the existing
+# 'beaper.LED2.value(1)' statement in GE 2, the microcontroller will turn
 # LED2 on first, followed by LED3. Though these two actions take place
-# sequentially, they happen so fast that, to us at least, it looks like both
-# LEDs turn on simultaneously.
+# sequentially, they happen so fast that, to us at least, it looks like
+# both LEDs turn on simultaneously.
 #
-# ## GE 4 - The main program loop
+# --------------------------------------------------------------------------------
+# GE 4 - The main program loop
+# --------------------------------------------------------------------------------
 #
 # If all that we wanted the program to do was to turn two LEDs on, it could
 # end after these two statements have run. In reality, microcontroller
@@ -136,54 +118,60 @@ while True:
 # were designed to do over and over again in what is called a main program
 # loop.
 #
-# In MicroPython programs, the `while True:` structure is the most common
-# method used to create a main program loop. Any statements indented by the
-# same amount below the `while True:` line will be repeated forever.
+# In MicroPython programs, the 'while True:' structure is the most common
+# method used to create a main program loop. Any statements indented by
+# the same amount below the 'while True:' line will be repeated forever.
 #
 # Indentation is very important in MicroPython, and program editors will
 # try to help you indent your program code in a consistent way - let them!
 # If two or more program statements that are meant to be in the same code
-# structure use different amounts of space characters to indent their code,
-# the MicroPython interpreter will generate an error due to the mismatch.
-# You will often run into this problem when copying and pasting code
-# examples from different sources.
+# structure use different amounts of space characters to indent their
+# code, the MicroPython interpreter will generate an error due to the
+# mismatch. You will often run into this problem when copying and pasting
+# code examples from different sources.
 #
-# Would it make sense to put the `beaper.LED2.value(1)` output statement
-# inside the `while True:` loop? Why or why not? What would the program be
+# Would it make sense to put the 'beaper.LED2.value(1)' output statement
+# inside the 'while True:' loop? Why or why not? What would the program be
 # trying to do if the statements used to turn on the LEDs were moved into
-# the `while True:` loop?
+# the 'while True:' loop?
 #
-# ## GE 5 - The pass statement
+# --------------------------------------------------------------------------------
+# GE 5 - The pass statement
+# --------------------------------------------------------------------------------
 #
-# The `pass` statement in the main `while True:` loop prevents MicroPython
-# from generating an error due to the otherwise empty loop. Comment out the
-# pass statement by placing a pound sign in front of the `pass` keyword.
+# The 'pass' statement in the main 'while True:' loop prevents MicroPython
+# from generating an error due to the otherwise empty loop. Comment out
+# the pass statement by placing a pound sign in front of the pass keyword.
 # Then, try to run the program.
 #
-# **Example code:**
-# ```python
+# Example code:
+#
 # # pass
-# ```
 #
 # Since MicroPython interpreters ignore the rest of any line of text
-# following a pound sign, the pass statement will now be ignored. 
-# What do you think the word `pass` actually does? How might `pass`
+# following a pound sign, the pass statement will now be ignored.
+# What do you think the word pass actually does? How might pass
 # be helpful to us later?
 #
-# ## GE 6 - Fast output changes
+# --------------------------------------------------------------------------------
+# GE 6 - Fast output changes
+# --------------------------------------------------------------------------------
 #
 # Let's try to blink LED3 on and off. Replace the contents of the main
-# `while True:` loop with two statements: one to turn the LED on, followed
+# 'while True:' loop with two statements: one to turn the LED on, followed
 # by a second one to turn the LED off.
 #
-# **Example code:**
-# ```python
-# while True:
-#   beaper.LED3.value(1)  # LED3 on
-#   beaper.LED3.value(0)  # LED3 off
-# ```
+# Example code:
 #
-# Since these two statements are now inside the main loop, `pass` is no
+# while True:
+#     beaper.LED3.value(1)  # LED3 on
+#     beaper.LED3.value(0)  # LED3 off
+#
+# Reminder: easily remove comment pound signs '#' from multiple lines of
+# code by highlighting the lines and pressing CMD-/ on macOS, or CTRL-/ on
+# Windows.
+#
+# Since these two statements are now inside the main loop, 'pass' is no
 # longer needed. The loop will run each statement once, first turning LED3
 # on, and then turning LED3 off. After that, the loop will repeat from the
 # top, turning LED3 on again, and then off again... forever.
@@ -192,7 +180,9 @@ while True:
 # should be happening? Is it? How could you test your prediction?
 
 
-# # Extension Activities
+# ================================================================================
+# Extension Activities
+# ================================================================================
 #
 # Extension activities give learners a chance to apply the concepts and
 # ideas presented in the guided exploration activities in new contexts,
@@ -200,30 +190,38 @@ while True:
 # practice, enrichment, or assessment. Independent learners can use them
 # to help solidify understanding and to apply their skills.
 #
-# ## EA 1 - Turn LED2 on and off
+# --------------------------------------------------------------------------------
+# EA 1 - Turn LED2 on and off
+# --------------------------------------------------------------------------------
 #
 # The statement used to turn LED2 on in the starter program runs before
 # the main while: loop. What do you think will happen if this statement
 # is immediately followed by a second statement to turn LED2 off?
 #
-# Try it! Do you see LED2 turning on and then off? Explain what is happening
-# and why you think this happens.
+# Try it! Do you see LED2 turning on and then off? Explain what is
+# happening and why you think this happens.
 #
-# ## EA 2 - Reverse the statement order
+# --------------------------------------------------------------------------------
+# EA 2 - Reverse the statement order
+# --------------------------------------------------------------------------------
 #
 # Predict what will happen if the order of the two LED2 statements in EA 1
 # is reversed so that LED2 is turned off first, and then turned on.
 #
 # Try it! Does the state of LED2 match your prediction?
 #
-# ## EA 3 - Create an LED pattern
+# --------------------------------------------------------------------------------
+# EA 3 - Create an LED pattern
+# --------------------------------------------------------------------------------
 #
-# Create a program that lights a pattern by illuminating at least two of the
-# on-board LEDs. Run your program to verify that it works as expected.
+# Create a program that lights a pattern by illuminating at least two of
+# the on-board LEDs. Run your program to verify that it works as expected.
 #
-# ## EA 4 - Use the microcontroller module LED
+# --------------------------------------------------------------------------------
+# EA 4 - Use the microcontroller module LED
+# --------------------------------------------------------------------------------
 #
-# The `BEAPER_Pico.py` board module file includes functions to control the
+# The BEAPER_Pico.py board module file includes functions to control the
 # Raspberry Pi Pico module's on-board LED, as well as many other pin
 # definitions and pre-made functions for the BEAPER Pico circuit. (Feel
 # free to open the file to explore all of the code that it contains.)
@@ -231,13 +229,11 @@ while True:
 # One of the functions in the BEAPER Pico board module is used to turn on
 # the Raspberry Pi Pico microcontroller's on-board LED:
 #
-# **Example code:**
-# ```python
+# Example code:
+#
 # beaper.pico_led_on()
-# ```
 #
 # Add this function call at the very beginning of your program so that
 # the Raspberry Pi Pico's LED can act as a status indicator to show that
 # your program is running even if none of the BEAPER Pico's on-board LEDs
 # are lit. Run the program to verify that it works.
-#
